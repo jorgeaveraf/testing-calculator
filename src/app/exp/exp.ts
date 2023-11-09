1a14eec359a6ec9211bb60e354bdda34ac22e7f5
JorgeVera <jorgeaveraf@gmail.com>
2023-11-08 23:00:59 -0600

    adition of img

--- src/app/exp/exp.ts
+++
@@ -1,7 +0,0 @@
-export function exp(base, exp) {  
-    // refactoring
-
-    let pow = 0;
-    pow = Math.pow(base, exp);
-    return pow;
-}
--- src/app/sqr/sqr.ts
+++
@@ -1,7 +0,0 @@
-export function sqr(oper1) {  
-    // refactoring
-    let sqr = 0
-     sqr = oper1 * oper1;
-
-    return sqr;
-}
